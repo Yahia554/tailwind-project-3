12336c0d6d0019c144631914755e3e7b95f3480f
yahia salman <mdyahiasalman624@gmail.com>
2022-12-22 04:16:47 +0600

    navbar responsive

---
+++ js/coustom.js
@@ -0,0 +1,13 @@
+const bar = document.getElementById("bar");
+const nav = document.getElementById("nav");
+
+bar.addEventListener("click", () => {
+  nav.classList.toggle("left-72");
+});
+
+// const bar = document.getElementById("bar");
+// const nav = document.getElementById("nav");
+
+// bar.addEventListener("click", () => {
+//   nav.classList.toggle("left-72");
+// });

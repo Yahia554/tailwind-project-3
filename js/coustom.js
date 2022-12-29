6314fa38cc0f07d2b38549ec53163ae5e8d5e7e6
yahia salman <mdyahiasalman624@gmail.com>
2022-12-30 05:03:26 +0600

    remove comment js file

--- js/coustom.js
+++ js/coustom.js
@@ -4,10 +4,3 @@ const nav = document.getElementById("nav");
 bar.addEventListener("click", () => {
   nav.classList.toggle("left-72");
 });
-
-// const bar = document.getElementById("bar");
-// const nav = document.getElementById("nav");
-
-// bar.addEventListener("click", () => {
-//   nav.classList.toggle("left-72");
-// });
